5cf9becdf155d0677b7a388238457d396bdac218
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-06 16:45:29 +0000

    failing test for initialing logparser

---
+++ lib/log_parser.rb
@@ -0,0 +1,7 @@
+class LogParser
+
+  def initialize
+    
+  end
+
+end
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,4 +1,11 @@
 RSpec.describe "LogParser" do
+
+  describe "initialize" do
+    it 'should initialize with an empty object' do
+      expect(LogParser.new.instance_variable_get(:@sorted_data_object)).to eq({})
+    end
+  end
+
   it 'should convert the file into an array'
   it 'should identify the url in each line'
   it 'should identify the ip address in each line'
