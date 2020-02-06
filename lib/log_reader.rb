d44796b4df928a5386525532ffe70951f680c2bf
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-06 16:41:28 +0000

    passing test for validating whether file is a log file

--- lib/log_reader.rb
+++ lib/log_reader.rb
@@ -2,6 +2,7 @@ class LogReader
 
   def run(file)
     return puts "File doesn't exist!" if file.nil? || !File.exists?(file)
+    return puts "File is not a log file!" if !file.path.include?(".log")
   end
 
 end
--- spec/log_reader_spec.rb
+++ spec/log_reader_spec.rb
@@ -8,8 +8,9 @@ RSpec.describe "LogReader" do
       expect{LogReader.new.run "/fake/file_path"}.to output(expected_output).to_stdout
     end
     it 'should return if the provided file is not a log file' do
+      tempfile = Tempfile.new([ 'foobar', '.txt' ])
       expected_output = "File is not a log file!\n"
-      expect{LogReader.new.run "/fake/file_path.txt"}.to output(expected_output).to_stdout
+      expect{LogReader.new.run tempfile}.to output(expected_output).to_stdout
     end
   end
 end
