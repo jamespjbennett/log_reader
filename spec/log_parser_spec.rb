df1acf6fc656f44486fe216b1451ac01c3e97d2e
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-06 20:56:30 +0000

    failing test for converting to array

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -3,8 +3,10 @@ require_relative '../lib/log_parser'
 RSpec.describe "LogParser" do
 
   before(:each) do
-    tempfile = Tempfile.new([ 'foobar', '.log' ])
-    @log_parser = LogParser.new(tempfile)
+    @tempfile = Tempfile.new([ 'foobar', '.log' ])
+    @tempfile.write("/help_page/1 126.318.035.038\n/contact 184.123.665.067\n/home 184.123.665.067\n/about/2 444.701.448.104")
+    @tempfile.read
+    @log_parser = LogParser.new(@tempfile)
   end
 
   describe "initialize" do
@@ -13,7 +15,7 @@ RSpec.describe "LogParser" do
     end
 
     it 'should initialize with a provided file instance variable' do
-      expect(@log_parser.instance_variable_get(:@file)).to eq(tempfile)
+      expect(@log_parser.instance_variable_get(:@file)).to eq(@tempfile)
     end
 
     it 'should fail to initialize without a file' do
@@ -22,9 +24,13 @@ RSpec.describe "LogParser" do
     end
   end
 
-  it 'should convert the file into an array' do
 
+  describe 'converting to array' do
+    it 'should convert the file into an array' do
+       expect(@log_parser.convert_to_array.class).to eq(array)
+    end
   end
+
 
   it 'should identify the url in each line'
   it 'should identify the ip address in each line'
