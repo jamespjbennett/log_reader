dbd457cf959eeac895772753cf43e8587f453f3e
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-06 08:27:11 +0000

    add initiak expectations and rspec gem

---
+++ spec/log_parser_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe "LogParser" do
+  it 'should convert the file into an array'
+  it 'should identify the url in each line'
+  it 'should identify the ip address in each line'
+  it 'should counts unique web address occurances'
+  it 'should count unique web address occurances'
+end
---
+++ spec/log_reader_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe "LogReader" do
+  describe 'validations' do
+    it 'should return if the provided file does not exist' do
+
+    end
+    it 'should return if the provided file is not a log file'
+  end
+end
