cc3653b8ddd697a686dd1233139682922679e343
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-06 21:21:26 +0000

    add rewind line to tempfile creation to allow liners to be read

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "LogParser" do
     @tempfile = Tempfile.new([ 'foobar', '.log' ])
     @tempfile.write("/help_page/1 126.318.035.038\n/contact 184.123.665.067\n/home 184.123.665.067\n/about/2 444.701.448.104")
     @tempfile.read
+    @tempfile.rewind
     @log_parser = LogParser.new(@tempfile)
   end
 
