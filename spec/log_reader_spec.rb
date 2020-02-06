802a3afc0649614c2529a1d91127e584292e5f03
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-06 08:28:57 +0000

    first failing test for validation existence of file in log reader

--- spec/log_reader_spec.rb
+++ spec/log_reader_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe "LogReader" do
   describe 'validations' do
     it 'should return if the provided file does not exist' do
-
+      expected_output = "File doesn't exist!"
+      expect(LogReader.new.run "/fake/file_path").to output(expected_output).to_stdout
     end
     it 'should return if the provided file is not a log file'
   end
