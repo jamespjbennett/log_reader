f4d564a90e95b1b1c7dceddfe4bf20d2d964cf03
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-07 08:13:03 +0000

    log array being converted to initial object

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -20,5 +20,10 @@ class LogParser
     line.match(/\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/).to_s
   end
 
+  def populate_sorted_data_object
+    log_array = convert_to_array
+    @sorted_data_object = log_array.each_with_object(Hash.new(0)) { |log_datum, hash_counter| hash_counter[log_datum] += 1 }
+  end
+
 
 end
