c1ad01a26f7692ef8ddbcc23b4ef4a9930f66e5e
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-08 22:07:09 +0000

    refactor data population per page visit

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -25,17 +25,28 @@ class LogParser
     log_array.each do |log_datum|
       url = get_url(log_datum)
       ip = get_ip(log_datum)
-      if @sorted_data_object[url]
-        @sorted_data_object[url][:page_visit_count] += 1
-        unique_page_visits = @sorted_data_object[url][:unique_page_visits]
-        unique_page_visits.push(ip) if !unique_page_visits.index(ip)
+      if data_exists_for_url(url)
+        increment_view_stats_for_url(url, ip)
       else
-        @sorted_data_object[url] = {}
-        @sorted_data_object[url][:page_visit_count] = 1
-        @sorted_data_object[url][:unique_page_visits] = [ip]
+        initialize_view_stats_for_url(url, ip)
       end
     end
   end
 
+  def data_exists_for_url(url)
+    @sorted_data_object[url]
+  end
+
+  def initialize_view_stats_for_url(url, ip)
+    @sorted_data_object[url] = {}
+    @sorted_data_object[url][:page_visit_count] = 1
+    @sorted_data_object[url][:unique_page_visits] = [ip]
+  end
+
+  def increment_view_stats_for_url(url, ip)
+    @sorted_data_object[url][:page_visit_count] += 1
+    unique_page_visits = @sorted_data_object[url][:unique_page_visits]
+    unique_page_visits.push(ip) if !unique_page_visits.index(ip)
+  end
 
 end
