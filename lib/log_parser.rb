cd079d3447c2c3e4cd7d9eb22afa08e24f20e365
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-09 14:52:27 +0000

    stripping back object building logic to approach a better way

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -22,31 +22,19 @@ class LogParser
 
   def populate_sorted_data_object
     log_array = convert_to_array
-    log_array.each do |log_datum|
-      url = get_url(log_datum)
-      ip = get_ip(log_datum)
-      if data_exists_for_url(url)
-        increment_view_stats_for_url(url, ip)
-      else
-        initialize_view_stats_for_url(url, ip)
-      end
-    end
+    uniq_urls_to_reference = log_array.map{|log_datum| get_url(log_datum)}.uniq
+
+
+    # log_array.each do |log_datum|
+    #   url = get_url(log_datum)
+    #   ip = get_ip(log_datum)
+    #   if data_exists_for_url(url)
+    #     increment_view_stats_for_url(url, ip)
+    #   else
+    #     initialize_view_stats_for_url(url, ip)
+    #   end
+    # end
   end
 
-  def data_exists_for_url(url)
-    @sorted_data_object[url]
-  end
-
-  def initialize_view_stats_for_url(url, ip)
-    @sorted_data_object[url] = {}
-    @sorted_data_object[url][:page_visit_count] = 1
-    @sorted_data_object[url][:unique_page_visits] = [ip]
-  end
-
-  def increment_view_stats_for_url(url, ip)
-    @sorted_data_object[url][:page_visit_count] += 1
-    unique_page_visits = @sorted_data_object[url][:unique_page_visits]
-    unique_page_visits.push(ip) if !unique_page_visits.index(ip)
-  end
 
 end
