6d7c25151eeb67741d0e5c2f937ab64481c345ec
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-09 16:28:57 +0000

    failing test for presenting page view stats

--- spec/log_presenter_spec.rb
+++ spec/log_presenter_spec.rb
@@ -5,7 +5,7 @@ require 'pry'
 RSpec.describe "LogPresenter" do
   before(:each) do
     @tempfile = Tempfile.new([ 'foobar', '.log' ])
-    @tempfile.write("/help_page/1 126.318.035.038\n/help_page/1 126.318.035.038\n/help_page/1 126.318.035.039\n/help_page/1 126.318.035.039\n/help_page/1 126.318.035.039\n/contact 184.123.665.067\n/contact 184.123.665.068\n/contact 184.123.665.069\n/home 184.123.665.065\n/home 184.123.665.065\n/home 184.123.665.065\n/about/2 444.701.448.104\n/about/2 444.701.448.105")
+    @tempfile.write("/help_page/1 126.318.035.038\n/help_page/1 126.318.035.038\n/help_page/1 126.318.035.039\n/help_page/1 126.318.035.039\n/help_page/1 126.318.035.039\n/contact 184.123.665.067\n/contact 184.123.665.068\n/contact 184.123.665.069\n/contact 184.123.665.069\n/home 184.123.665.065\n/home 184.123.665.065\n/home 184.123.665.065\n/about/2 444.701.448.104\n/about/2 444.701.448.105")
     @tempfile.read
     @tempfile.rewind
     @log_parser = LogParser.new(@tempfile)
@@ -18,6 +18,7 @@ RSpec.describe "LogPresenter" do
   describe 'ordering by page views' do
     before(:each) do
       @sorted_by_page_view = @log_presenter.page_view_sort
+      @present_sorted_by_page_view = @log_presenter.present_page_view_sort_stats
     end
 
     it 'should return a hash' do
@@ -28,6 +29,11 @@ RSpec.describe "LogPresenter" do
     it 'should sort by page views' do
       expect(@sorted_by_page_view.keys.first).to eq("/help_page/1")
       expect(@sorted_by_page_view.keys.last).to eq("/about/2")
+    end
+
+    it 'should print out formatted ordering of log lage view stats' do
+      expected_output = "/help_page/1 5 visits\n/contact 4 visits\n/home 3 visits\n/about/2 2 visits"
+      expect{@present_sorted_by_page_view}.to output(expected_output).to_stdout
     end
   end
 
@@ -45,4 +51,6 @@ RSpec.describe "LogPresenter" do
     end
 
   end
+
+
 end
