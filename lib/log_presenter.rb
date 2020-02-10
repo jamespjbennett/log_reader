adcf88e4c83894b027733b9aaef451672e09786e
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-10 20:04:54 +0000

    move logic into inherited logpresenter classes

--- lib/log_presenter.rb
+++ lib/log_presenter.rb
@@ -4,14 +4,10 @@ class LogPresenter
     @log_data =log_data
   end
 
-  def page_view_sort
-    @log_data.sort{|a,b| b[1][:page_visit_count] <=> a[1][:page_visit_count]}.to_h
-  end
+end
 
-  def present_page_view_sort_stats
-    page_view_sort.each{|url, stats| puts "#{url} #{stats[:page_visit_count]} visits"}
-  end
 
+class UniqPageViewPresenter < LogPresenter
   def uniq_page_view_sort
     @log_data.sort{|a,b| b[1][:unique_page_visits].length <=> a[1][:unique_page_visits].length}.to_h
   end
@@ -19,13 +15,14 @@ class LogPresenter
   def present_uniq_page_view_sort_stats
     uniq_page_view_sort.each{|url, stats| puts "#{url} #{stats[:unique_page_visits].length} unique views"}
   end
-
-end
-
-
-class UniqPageViewPresenter < LogPresenter
-
 end
 
 class TotalPageViewPresenter < LogPresenter
+  def page_view_sort
+    @log_data.sort{|a,b| b[1][:page_visit_count] <=> a[1][:page_visit_count]}.to_h
+  end
+
+  def present_page_view_sort_stats
+    page_view_sort.each{|url, stats| puts "#{url} #{stats[:page_visit_count]} visits"}
+  end
 end
