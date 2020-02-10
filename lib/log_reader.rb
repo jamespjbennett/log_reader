3ae161e1026fceaeff9a136cc7296f3a4f38cd29
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-10 13:05:58 +0000

    neatening up log reader class

--- lib/log_reader.rb
+++ lib/log_reader.rb
@@ -6,14 +6,10 @@ class LogReader
   def run(file)
     return puts "File doesn't exist!" if file.nil? || !File.exists?(file)
     return puts "File is not a log file!" if !file.include?(".log")
-    begin
-      @log_parser = LogParser.new(file)
-      @log_parser.populate_sorted_data_object
-      LogPresenter.new(@log_parser.sorted_data_object).present_page_view_sort_stats
-      LogPresenter.new(@log_parser.sorted_data_object).present_uniq_page_view_sort_stats
-    rescue Exception => e
-      puts e
-    end
+    @log_parser = LogParser.new(file)
+    @log_parser.populate_sorted_data_object
+    LogPresenter.new(@log_parser.sorted_data_object).present_page_view_sort_stats
+    LogPresenter.new(@log_parser.sorted_data_object).present_uniq_page_view_sort_stats
   end
 
 end
