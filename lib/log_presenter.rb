ceab65971d58acd56b3e1a56b41589c3587088af
jamespjbennett <jamespjbennett@hotmail.com>
2020-02-10 20:05:32 +0000

    rename methods to be common throughout log presenter childclasses

--- lib/log_presenter.rb
+++ lib/log_presenter.rb
@@ -8,21 +8,23 @@ end
 
 
 class UniqPageViewPresenter < LogPresenter
-  def uniq_page_view_sort
+  
+  def sort_data
     @log_data.sort{|a,b| b[1][:unique_page_visits].length <=> a[1][:unique_page_visits].length}.to_h
   end
 
-  def present_uniq_page_view_sort_stats
+  def present
     uniq_page_view_sort.each{|url, stats| puts "#{url} #{stats[:unique_page_visits].length} unique views"}
   end
 end
 
 class TotalPageViewPresenter < LogPresenter
-  def page_view_sort
+
+  def sort_data
     @log_data.sort{|a,b| b[1][:page_visit_count] <=> a[1][:page_visit_count]}.to_h
   end
 
-  def present_page_view_sort_stats
+  def present
     page_view_sort.each{|url, stats| puts "#{url} #{stats[:page_visit_count]} visits"}
   end
 end
